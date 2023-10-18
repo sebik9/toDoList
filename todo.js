0fec8ecb8c1b84ee830c16a27a099e413002f95a
sebik9 <s3bik9@seznam.cz>
2023-10-18 23:50:24 +0200

    Update todo.js
    added better comments

--- todo.js
+++ todo.js
@@ -1,5 +1,7 @@
+//global declaration for X symbol later used
 const CLOSE_ICON = "\u00D7";
 
+//functio that appends close button to created element accepts li as element
 function createCloseButton(element) {
     const span = document.createElement("SPAN");
     span.className = "close";
@@ -11,13 +13,15 @@ function createCloseButton(element) {
     };
 }
 
+//function that adds new elements as task in to do list
 function addNewElement() {
+    //takes html input and stores it in inputValue
     const inputValue = document.getElementById("myInput").value;
     if (inputValue.trim() === '') {
         showError("You must write something!");
         return;
     }
-
+    //creates new li element in selected UL
     const li = document.createElement("li");
     li.appendChild(document.createTextNode(inputValue));
     document.getElementById("myUL").appendChild(li);
